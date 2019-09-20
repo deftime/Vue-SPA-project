d4389a234228dd4cd3067d42d13bb86476fada8f
Volodymyr Gorbunov <deftime@gmail.com>
2019-09-20 12:53:03 +0300

    JS.Firebase save answers

--- js/editqu.js
+++ js/editqu.js
@@ -11,16 +11,28 @@
   firebase.initializeApp(firebaseConfig);
 })();
 
-let db = firebase.database();
+var db = firebase.database();
+var auth = firebase.auth();
+
+auth.onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = 'index.html';
+  }
+})
 
 let form = document.forms.quForm;
 let clientName = document.querySelector('#clientName');
 let clientMail = document.querySelector('#clientMail');
 let simpleMsg = document.querySelector('#simpleMsg');
 let proMsg = document.querySelector('#proMsg');
+let save = document.querySelector('#saveQu');
+let rezMsg = document.querySelector('#rezSave');
 
+// Get URL for take id of question
 let url = new URL(window.location.href);
 let id = url.search.slice(4);
+
+// Take question from base
 
 document.addEventListener('DOMContentLoaded', () => {
   let objObj;
@@ -45,5 +57,35 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .catch(error => {
       console.log('Не удалось получить данные с базы: ' + error.message);
+    })
+})
+
+// Save answer to base
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+})
+
+save.addEventListener('click', () => {
+  let upObj = {};
+  upObj.answer = form.ans.value;
+  upObj.author = auth.currentUser.email.slice(0, auth.currentUser.email.indexOf('@'));
+  if (upObj.answer != '') {
+    upObj.answerflag = true;
+  } else {
+    upObj.answerflag = false;
+  }
+
+  db.ref('Questions').child(id).update(upObj)
+    .then(() => {
+      rezMsg.innerText = 'Відповідь записана';
+      rezMsg.style.color = 'green';
+      setTimeout(() => {rezMsg.innerText = ''}, 2000);
+    })
+    .catch(err => {
+      rezMsg.innerText = 'Зберегти не вдалося!';
+      rezMsg.style.color = 'red';
+      setTimeout(() => {rezMsg.innerText = ''}, 2000);
+      console.log(err.message);
     })
 })
